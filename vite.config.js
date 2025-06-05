ff20606aee9535c425ebdf4ece5af4f1aa37b907
jwvdriel <janwillemvandriel@gmail.com>
2025-06-05 22:19:13 +0200

    added comment to config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // github actions and setting base url
   base: 'https://jwvdriel.github.io/testimonial-page/',
+
+  // The Vue plugin will parse absolute URLs and treat them
+  // as absolute paths to files on disk. Setting this to
+  // `false` will leave absolute URLs un-touched so they can
+  // reference assets in the public directory as expected.
   includeAbsolute: false,
 });
