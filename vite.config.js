7100e2fba818e07e9e843546e5aa82d34c958bfc
jwvdriel <janwillemvandriel@gmail.com>
2025-06-05 22:16:36 +0200

    last update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://jwvdriel.github.io/testimonial-page/',
+  base: 'https://jwvdriel.github.io/testimonial-page/',
   includeAbsolute: false,
 });
