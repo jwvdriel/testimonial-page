f9e0c0595cb8abb472042a8c4eb67805c7e08966
jwvdriel <janwillemvandriel@gmail.com>
2025-06-05 19:09:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <>
+      <div className='main-container'>
+        <div className='card'>
+          <div className='profile'>
+            <img src='/Profile Thumbnail.svg' alt='profile image' />
+            <div className='text-pair'>
+              <figcaption className='profile-name'>Sarah Dole</figcaption>
+              <figcaption className='profile-text'>@sarahdole</figcaption>
+            </div>
+          </div>
+          <span>
+            I&apos;ve been searching for high-quality abstract images for my
+            design projects, and I&apos;m thrilled to have found this platform.
+            The variety and depth of creativity are astounding!
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
